6da3a0c637eb23ac1123633dd2235a88e70baa0b
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-11-12 10:43:57 -0700

    Add functionality to univite button

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var invitationList = document.querySelector('.main-section-invitation-list');
 var inviteBtn = document.querySelector('#invite-btn');
 var nameInput = document.querySelector('#name-input');
 
+
 inviteBtn.addEventListener('click', addHedgehog);
+invitationList.addEventListener('click', uninviteHedgehog);
 
 function addHedgehog() {
   invitationList.innerHTML +=
@@ -17,4 +19,10 @@ function addHedgehog() {
     allergiesInput.value = 'Allergies';
     hogletsInput.value = 'Number of Hoglets';
     nameInput.value = 'Name of Hedgehog';
+}
+
+function uninviteHedgehog(event) {
+  if(event.target.parentElement.parentElement.children[2].childNodes[7].id === 'uninvite-btn') {
+    event.target.parentElement.remove()
+  }
 }
