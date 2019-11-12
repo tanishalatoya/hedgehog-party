fbaa16c51732da0cc2ce235297ca1795c4c77044
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-11-11 18:46:37 -0700

    Replace value with default after invitation btn is clicked

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-// Target the section where the div (each invite) should be added to the page once the input has been filled in
-
-// Need to take input values and put them in the invite List
-
-var nameInput = document.querySelector('#name-input');
-var hogletsInput = document.querySelector('#hoglets-input');
 var allergiesInput = document.querySelector('#allergies-input');
-var inviteBtn = document.querySelector('#invite-btn');
+var hogletsInput = document.querySelector('#hoglets-input');
 var invitationList = document.querySelector('.main-section-invitation-list');
+var inviteBtn = document.querySelector('#invite-btn');
+var nameInput = document.querySelector('#name-input');
 
 inviteBtn.addEventListener('click', addHedgehog);
 
@@ -18,7 +14,7 @@ function addHedgehog() {
       <p id="allergies">${allergiesInput.value}</p>
       <button id="uninvite-btn" type="button" name="uninvite">Uninvite</button>
     </div>`
-    nameInput.value = '';
-    hogletsInput.value = '';
-    allergiesInput.value = '';
+    allergiesInput.value = 'Allergies';
+    hogletsInput.value = 'Number of Hoglets';
+    nameInput.value = 'Name of Hedgehog';
 }
