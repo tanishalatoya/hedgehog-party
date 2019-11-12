b667b8e892bd392b05c4ead412ad7838cedc7d6d
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-11-11 18:32:34 -0700

    Add functionality to add guests to the invitation list

--- main.js
+++ main.js
@@ -2,14 +2,23 @@
 
 // Need to take input values and put them in the invite List
 
-var name = document.querySelector('#name');
-var hoglets = document.querySelector('#hoglets');
-var allergies = document.querySelector('#allergies');
+var nameInput = document.querySelector('#name-input');
+var hogletsInput = document.querySelector('#hoglets-input');
+var allergiesInput = document.querySelector('#allergies-input');
 var inviteBtn = document.querySelector('#invite-btn');
-var invitationList = document.querySelector('.invitation-list-items');
+var invitationList = document.querySelector('.main-section-invitation-list');
 
 inviteBtn.addEventListener('click', addHedgehog);
 
 function addHedgehog() {
-  console.log('It works');
+  invitationList.innerHTML +=
+    `<div class="invitation-list-items">
+      <p id="name">${nameInput.value}</p>
+      <p id="hoglets">${hogletsInput.value}</p>
+      <p id="allergies">${allergiesInput.value}</p>
+      <button id="uninvite-btn" type="button" name="uninvite">Uninvite</button>
+    </div>`
+    nameInput.value = '';
+    hogletsInput.value = '';
+    allergiesInput.value = '';
 }
